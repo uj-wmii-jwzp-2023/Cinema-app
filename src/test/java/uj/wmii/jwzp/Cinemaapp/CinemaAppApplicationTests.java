3038f315058cb2e33cdb5aa19808a75cb75d6262
GrzegorzZurek <grzegorz.zurek@student.uj.edu.pl>
2023-03-29 17:44:46 +0200

    Spring initializr - Spring web

---
+++ src/test/java/uj/wmii/jwzp/Cinemaapp/CinemaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uj.wmii.jwzp.Cinemaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
